6977737e3d7de33538d0b942f4db4e7ee9e29e04
LouieDSG <107040545+LouieDSG@users.noreply.github.com>
2022-11-28 01:02:34 +0000

    For PC

--- BrainStormDev.js
+++ BrainStormDev.js
@@ -1,3 +1,5 @@
+let Noise = 0.1;
+
 function setup () {
 
     createCanvas(600, 600);
@@ -6,19 +8,23 @@ function setup () {
 
 function draw () {
 
-    background(0);
+    background(0,10);
     translate(width/2, height/2);
     stroke(255, 0, 0)
     noFill();
 
-    let t = 0;
     beginShape();
 
     for (let a = 0; a < TWO_PI; a += 0.1) {
-        let r = noise(t) * 100;
+        let xoff = map(cos(a), -1, 1, 0, Noise);
+        let yoff = map(sin(a), -1, 1, 0, Noise);
+        let r = map(noise(xoff, yoff), 0, 2, 100, 200); //the perlin noise range is always between 0 and 1
         let x = r * cos (a);
         let y = r * sin (a);
         vertex (x, y);
+
     }
     endShape(CLOSE);
+
+    Noise += 0.05
 }
